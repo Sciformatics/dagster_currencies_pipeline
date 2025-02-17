baea95d651c7b54de6e6af1a55557116c1c0f9bb
Alexander McDougall-Morrison <a.mcdougallmorrison@gmail.com>
2025-02-17 21:54:42 +0000

    updated cron job time to every minute, fixed bug with the asset check for nulls test

--- dagster_pipeline__currencies/assets.py
+++ dagster_pipeline__currencies/assets.py
@@ -1,23 +1,33 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                       IMPORT MODULES
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-from dagster import asset
-from constants import POSTGRES_PATH
-import requests, psycopg2
-from datetime import datetime
+from dagster import asset, asset_check, AssetCheckResult
+import requests
+from datetime import datetime, timedelta
 from sqlalchemy import create_engine
+from constants import POSTGRES_PATH  # Set your own Postgres instance connection in your constants file
 import pandas as pd
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                       CONSTANTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-engine = create_engine(POSTGRES_PATH)
+ENGINE = create_engine(POSTGRES_PATH)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                       ASSETS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 @asset(
     description="The raw extract from the CoinCap Rates API."
 )
-def all_currencies() -> None:
+def all_currencies():
 
     response = requests.get('https://api.coincap.io/v2/rates').json()
     response_df = pd.json_normalize(response['data'])
     response_df['api_call_at'] = datetime.now()
-    response_df.to_sql('all_currencies', engine, if_exists='append', index=False)
+    response_df.to_sql('all_currencies', ENGINE, if_exists='append', index=False)
 
 @asset(
     deps=["all_currencies"],
@@ -26,14 +36,13 @@ def all_currencies() -> None:
 def crypto_currencies():
 
     query = '''
-
-        SELECT * FROM ALL_CURRENCIES
+        
+        SELECT * FROM all_currencies
         WHERE TYPE ILIKE 'CRYPTO';
     
     '''
-    crypto_df = pd.read_sql(query, engine)
-    crypto_df.to_sql('crypto_currencies', engine, index=False)
-
+    crypto_df = pd.read_sql(query, ENGINE)
+    crypto_df.to_sql('crypto_currencies', ENGINE, if_exists='replace', index=False)
 
 @asset(
     deps=["all_currencies"],
@@ -42,10 +51,28 @@ def crypto_currencies():
 def fiat_currencies():
 
     query = '''
-
-        SELECT * FROM ALL_CURRENCIES
+    
+        SELECT * FROM all_currencies
         WHERE TYPE ILIKE 'FIAT';
     
     '''
-    crypto_df = pd.read_sql(query, engine)
-    crypto_df.to_sql('fiat_currencies', engine, index=False)
+    fiat_df = pd.read_sql(query, ENGINE)
+    fiat_df.to_sql('fiat_currencies', ENGINE, if_exists='replace', index=False)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                       ASSET TESTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Checks for nulls in ID column
+
+@asset_check(asset=all_currencies)
+def test_for_nulls_in_currencies_df():
+    query = '''
+        SELECT * FROM all_currencies
+        WHERE ID IS NOT NULL AND SYMBOL IS NOT NULL;'''
+    check_for_nulls_df = pd.read_sql(query, ENGINE)
+    number_of_nulls = check_for_nulls_df['id'].isna().sum()
+
+    return AssetCheckResult(
+        passed=bool(number_of_nulls == 0),
+    )
--- dagster_pipeline__currencies/definitions.py
+++ dagster_pipeline__currencies/definitions.py
@@ -1,9 +1,26 @@
-from dagster import Definitions, load_assets_from_modules
-
+from dagster import Definitions, load_assets_from_modules, AssetSelection
+from dagster_pipeline__currencies.assets import test_for_nulls_in_currencies_df
 from dagster_pipeline__currencies import assets  # noqa: TID252
+from dagster import define_asset_job, ScheduleDefinition
+
 
 all_assets = load_assets_from_modules([assets])
 
+currency_api_refresh_job = define_asset_job(
+    "per_minute_refresh", selection=AssetSelection.assets(*all_assets),
+)
+
+hourly_schedule = ScheduleDefinition(
+    job=currency_api_refresh_job,
+    cron_schedule="* * * * *", # on the minute, every minute, every hour, every day
+)
+
+
 defs = Definitions(
     assets=all_assets,
+    asset_checks=[test_for_nulls_in_currencies_df],
+    jobs=[currency_api_refresh_job],
+    schedules=[hourly_schedule]
 )
+
+
